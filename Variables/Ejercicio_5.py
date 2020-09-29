20a77a9e2cd9247ea0827a81d3038e7c6add98eb
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-09-29 16:52:25 -0400

    Mostrando la paga total por hora trabajada

---
+++ Variables/Ejercicio_5.py
@@ -0,0 +1,8 @@
+saludo = "¡Hola!,"
+nombre = input("¿Como te llamas?")
+horas_trabajadas = input("¿Cuantas horas trabajaste?")
+pago_por_hora = "Son 70 Bolivianos la hora"
+monto_a_cobrar = 70*int(horas_trabajadas)
+mensaje = "tu monto a cobrar es:"
+B = "Bolivianos"
+print(saludo, nombre, pago_por_hora, mensaje, monto_a_cobrar, B)
