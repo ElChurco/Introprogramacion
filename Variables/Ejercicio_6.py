017505ac78f344aec3b5021f2ae0b4e6ca15d4f5
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-09-29 17:13:53 -0400

    Mostrando la masa corporal

---
+++ Variables/Ejercicio_6.py
@@ -0,0 +1,5 @@
+saludo = "Hola!"
+peso = input("¿Cual es tu peso actual?")
+altura = float(input("¿Cual es tu estatura actual?"))
+masa_corporal = int(peso)/float(altura)
+print(saludo, "Tu indice corporal es de", masa_corporal)
