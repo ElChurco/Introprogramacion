6647b98c3803d49cf0179e5e9c23781369aedde5
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-01 17:06:53 -0400

    Mostrando los valores

---
+++ Variables2/Ejercicio4.py
@@ -0,0 +1,10 @@
+n1 = int(input("Ingrese el Primer numero: "))
+n2 = int(input("Ingrese el Segundo numero: "))
+iguales = n1 == n2
+print(f"Los numeros son iguales: {iguales}")
+distintos = n1 != n2
+print(f"Los numeros son distintos: {distintos}")
+n1_mayor_que_n2 = n1>n2
+print(f"n1 es mayor que n2: {n1_mayor_que_n2}")
+n1_menor_o_igual_n2 = n1<=n2
+print(f"n1 es menor o igual que n2: {n1_menor_o_igual_n2}")
