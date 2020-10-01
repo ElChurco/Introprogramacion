fc3b7808303bdb618a4e9ed0a7d0e57ec4325b36
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-01 16:24:17 -0400

    Mostrando el valor del resto

---
+++ Variables2/Ejercicio2.py
@@ -0,0 +1,4 @@
+dividendo = int(input("Ingrese el dividendo: "))
+divisor = int(input("ingrese el divisor: "))
+resto = dividendo % divisor
+print("El resto obtenido al dividir", dividendo, "entre", divisor, "es", resto)
