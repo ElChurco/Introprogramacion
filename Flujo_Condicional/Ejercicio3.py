997d975c62b9c4345bde5c6023e16d43ab97d742
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-02 16:50:42 -0400

    Mostrando partidos politicos

---
+++ Flujo_Condicional/Ejercicio3.py
@@ -0,0 +1,8 @@
+p1 = "MNR"
+p2 = "MAS"
+print(p1, p2)
+voto = input("Seleccione un partido politico: ")
+if voto == p1 or voto == p2 :
+    print("Usted acaba de votar")
+else:
+    print("Voto invalido")
