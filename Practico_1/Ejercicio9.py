99d72fa86093056f52e1981c16ef7311ab07f1a9
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-12 14:04:25 -0400

    Mostrando una calculadora basica

---
+++ Practico_1/Ejercicio9.py
@@ -0,0 +1,12 @@
+n1 = float(input("Por favor ingrese un numero: "))
+n2 = float(input("Por favor ingrese un segundo numero: "))
+res = n1 + n2
+print("La suma es: ", res)
+res2 = n1 - n2
+print("La resta es: ",res2)
+res3 = n1 * n2
+print("La multiplicacion es: ", res3)
+res4 = n1 / n2
+print("La division es: ",res4)
+print("Gracias!")
+
