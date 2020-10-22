26b41d8d93ea56598a91ffadb8d4d825c88032d8
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-22 16:29:46 -0400

    Mostrando las tablas de multiplicar

---
+++ Funciones/Tablas_de_multiplicar.py
@@ -0,0 +1,23 @@
+#def tablaM(numI):
+    #for i in range(1,11):
+        #print(numI, "x", i, "=", i * numI)
+
+#num = int(input("Ingrese un numero: "))
+#tablaM(num)
+def tablaM(num):
+    print(f"Tabla de multiplicar de {num}")
+
+    for j in range(1,11):
+        print(f"{num} x {j} = {num * j}")
+
+tablaM(1)
+tablaM(2)
+tablaM(3)
+tablaM(4)
+tablaM(5)
+tablaM(6)
+tablaM(7)
+tablaM(8)
+tablaM(9)
+tablaM(10)
+print("GRACIAS!")
