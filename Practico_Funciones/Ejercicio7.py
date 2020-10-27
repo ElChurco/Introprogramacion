ba87bd4f232abccd586423fb5fd168e227a348d8
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-10-27 12:18:11 -0400

    Mostrando n cantidad de caracteres y multiplicandolos

---
+++ Practico_Funciones/Ejercicio7.py
@@ -0,0 +1,6 @@
+p1 = input("Por favor ingrese una letra: ")
+num = input("Por favor ingrese un numero: ")
+def num_caracteres(x, y):
+    op = int (x) * str(y)
+    print(op)
+num_caracteres(num, p1)
