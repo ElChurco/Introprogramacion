7f1a3ec1e2adaa8d59b940e2e13d19e456f0723c
Andres La Madrid <andres.lamadrid@ucb.edu.bo>
2020-11-27 16:51:35 -0400

    Mostrando el programa desarrollado

--- Grafica/Ejercicio_3.py
+++ Grafica/Ejercicio_3.py
@@ -1,20 +1,18 @@
 import random
+def crearVectorRandom(tam):
+    vector=[]
+    for i in range(1,tam+1):
+        vector.append(random.randint(1,300))
+    return vector
 
-num = int(input("Ingrese el tamaño de su Lista: "))
-contador = 1
-vector2 = []
-vector3 = []
-
-while contador <= num:
-    v1 = random.randint(1,300)
-    a = str(v1)
-    if a[-1] == str(5):
-        vector3.append(v1)
-    vector2.append(v1)
-    contador = contador + 1
-
-
-print("")
-print(f"La lista aleatoria de {num} es: \n{vector2}")
-print("")
-print(f"Numeros a lazar terminados en 5: \n{vector3}")
+tam=int(input("Ingrese el tamaño del vector: "))
+vectorNumerosAleatorios=crearVectorRandom(tam)
+vectorNumerosEncontrados=[]
+print(vectorNumerosAleatorios)
+digito=int(input("Ingrese el último dígito a verificar: "))
+for pos in range(0,tam):
+    valor=vectorNumerosAleatorios[pos]
+    residuo=valor%10
+    if(residuo==digito):
+        vectorNumerosEncontrados.append(valor)
+print(vectorNumerosEncontrados)
